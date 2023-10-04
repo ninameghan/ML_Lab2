be71918b706a0516f2e63a876a110f00354658e8
lemmenn <nina.lemmens@hva.nl>
2023-10-04 14:48:23 +0200

    Task 1 - min & max temp

--- main.py
+++ main.py
@@ -8,16 +8,25 @@ def main():
 
 def bike():
     data = pd.read_csv("data/day.csv")
+
+    # Holiday vs Non-holiday rentals
     non_holiday = data[data["holiday"] == 0][["casual", "registered"]]
     holiday = data[data["holiday"] == 1][["casual", "registered"]]
-
-    # Print results:
     print("Non-Holiday rentals:")
     print()
     print(non_holiday.mean())
     print("Holiday rentals:")
     print()
     print(holiday.mean())
+    print()
+
+    # Min & Max temperature
+    temperatures = data["temp"] * (39 - (-8)) + (-8)
+    print("Minimum temperature: ", temperatures.min())
+    print("Maximum temperature: ", temperatures.max())
+    print()
+
 
 
 main()
+
